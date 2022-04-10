860f6f12adbdd5d385819ff43bbb8c31bd39a8a0
James <grimmett.james@gmail.com>
2022-04-10 13:40:04 +1000

    update docstrings and error handling

--- simple_gce/io/check_stellar_models.py
+++ simple_gce/io/check_stellar_models.py
@@ -6,17 +6,7 @@ import pandas as pd
 
 from .. import config
 from ..gce import approx_lifetime, approx_winds
-from ..utils import error_handling
 from ..utils.chem_elements import el2z
-
-# TODO:
-# check mass limits provided in config.
-# def check_winds(df):
-#   models with wind and CC, check consistency of mass / mass_final
-#   models with CC and no wind, check mass == mass_final
-
-# def check_ia(df):
-#   check consistency with config params
 
 REQUIRED_COLUMNS = [
     "mass",  # ZAMS mass
@@ -57,14 +47,21 @@ def check_initial(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-def check_columns(df: pd.DataFrame):
-    """
-    Check that all of the necessary columns are present. Where possible, fill
-    missing columns appropriately.
-    """
+def check_columns(df: pd.DataFrame) -> None:
+    """Check that all of the necessary columns are present.
 
-    # TODO:
-    # - Approximate and fill remnant_mass, mass_presn, if missing.
+    Where possible, fill missing columns appropriately.
+
+    Parameters
+    ----------
+    df: pd.DataFrame
+        Dataframe containing stellar models.
+
+    Raises
+    -------
+    ValueError: If required columns are missing from input data, or if there are
+        unknown columns in the data.
+    """
 
     # Columns excluding required
     non_req_columns = set(df.columns).difference(set(REQUIRED_COLUMNS))
@@ -72,35 +69,58 @@ def check_columns(df: pd.DataFrame):
     remaining_columns = non_req_columns.difference(set(OPTIONAL_COLUMNS))
     # Columns excluding required, optional, and chemical elements
     unknown_columns = remaining_columns.difference(set(el2z.keys()))
+    # Required columns that are missing
+    missing_columns = list(set(REQUIRED_COLUMNS) - set(df.columns))
 
-    if not set(REQUIRED_COLUMNS).issubset(set(df.columns)):
-        raise error_handling.InsufficientDataError("Stellar data is missing required columns.")
+    if len(missing_columns) != 0:
+        raise ValueError(f"Stellar data is missing required columns:\n{missing_columns}")
     elif len(unknown_columns) != 0:
-        raise error_handling.UnknownCaseError(
-            f"Unknown columns/elements in stellar data.\n{unknown_columns}"
-        )
+        raise ValueError(f"Unknown columns/elements in stellar data:\n{unknown_columns}")
 
 
 def check_missing_vals(df: pd.DataFrame) -> pd.DataFrame:
-    """Check that all required data is present."""
+    """Check that all required data is present.
 
+    `mass`, `mass_final`, `remnant_mass`, `type`, and `Z` must be filled for each row.
+    `lifetime` can be NaN, it will be approximated from mass and metallicity.
+
+    Parameters
+    ----------
+    df: pd.DataFrame
+        Dataframe containing stellar models.
+
+    Returns
+    -------
+    pd.DataFrame
+        The input dataframe with filled lifetime values if they were NaN.
+
+    Raises
+    ------
+    ValueError: If any required column contains NaNs
+    """
+
+    if not df[df["mass"].isna()].empty:
+        raise ValueError(
+            "Missing some entries for mass. This column should contain ZAMS for each model."
+        )
+    if not df[df["type"].isna()].empty:
+        raise ValueError(
+            "Missing some entries for type. This column should contain a string indicating "
+            "the type of each model, i.e., one of 'cc', 'cc_wind', 'agb', 'hn', or 'hn_wind'."
+        )
     if not df[df["mass_final"].isna()].empty:
-        raise error_handling.InsufficientDataError(
+        raise ValueError(
             "Missing some entries for mass_final. "
             "If there are no winds/mass loss in a model, set mass_final = mass."
         )
     if not df[df["remnant_mass"].isna()].empty:
-        raise error_handling.InsufficientDataError(
+        raise ValueError(
             "Missing some entries for remnant_mass. "
             "If there is no remnant (e.g. SNe Ia), set this column to zero. "
             "For AGB stars, set mass_remnant = mass_final"
         )
     if not df[df["Z"].isna()].empty:
-        raise error_handling.InsufficientDataError(
-            "Missing some entries for Z (metallicity). "
-            "If metallicity is not relevant, or you have models with a single value "
-            "of metallicity, set this column to zero for those models."
-        )
+        raise ValueError("Missing some entries for Z (metallicity).")
     if not df[df["lifetime"].isna()].empty:
         warnings.warn("Missing stellar lifetime data. Filling with approx values.")
         df = approx_lifetime.fill_lifetimes(df)
@@ -109,20 +129,42 @@ def check_missing_vals(df: pd.DataFrame) -> pd.DataFrame:
 
 
 def check_model_types(df: pd.DataFrame) -> pd.DataFrame:
-    # Types may include core-collapse, asymptotic giant-branch, thermonuclear,
-    # and hypernovae.
+    """
+    Model types may include core-collapse (cc), asymptotic giant-branch (agb),
+    thermonuclear (ia), and hypernovae (hn). Core-collapse and hypernovae models
+    may also have wind components (cc_wind, hn_wind).
+
+    Models must include core-collapse. If AGB models are not included, they will
+    be approximated by recycling the ISM compisition from time of birth. Wind
+    components for CCSNe and HNe will likewise be approximated if they are not included.
+
+    Parameters
+    ----------
+    df: pd.DataFrame
+        Dataframe containing stellar models.
+
+    Returns
+    -------
+    pd.DataFrame
+        The input dataframe, with AGB models added if they were missing.
+
+    Raises
+    ------
+    ValueError: If cc models are not present, or unknown models types are present.
+    """
     known_types = ["cc", "agb", "hn", "cc_wind", "hn_wind"]
 
     types = df.type.unique()
 
     if not set(types).issubset(set(known_types)):
         unknown_types = list(set(types).difference(set(known_types)))
-        raise error_handling.UnknownCaseError(f"Unknown model type/s : {unknown_types}")
+        raise ValueError(
+            f"Unknown model type/s : {unknown_types}. \n"
+            f"Model types must be one of {known_types}."
+        )
 
     if "cc" not in types:
-        raise error_handling.InsufficientDataError(
-            'Stellar models must include core-collapse (type = "CC")'
-        )
+        raise ValueError('Stellar models must include core-collapse (type = "CC")')
 
     if "agb" not in types:
         mass_min_cc = config.STELLAR_MODELS["mass_min_cc"]
@@ -136,60 +178,107 @@ def check_model_types(df: pd.DataFrame) -> pd.DataFrame:
 
     if config.STELLAR_MODELS["include_hn"] is True:
         if "hn" not in types:
-            message = "HNe are not included."
-            warnings.warn(message)
+            warnings.warn(
+                "HNe are not included in model input data, "
+                "though include_hn is specified in config."
+            )
     else:
-        if "hn" in types:
-            message = "Config is set to exclude HNe. Removing HNe models."
-            warnings.warn(message)
-            df = df[df.type != "hn"]
+        if "hn" in types or "hn_wind" in types:
+            warnings.warn("Config is set to exclude HNe. Removing HNe models.")
+            df = df[(df.type != "hn") & (df.type != "hn_wind")]
 
     return df
 
 
 def check_massfracs(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    The composition of ejecta must sum to one, i.e. the composition must be provided in
+    mass fraction of the ejecta. If there is a small error (<=1.e-3), scaling will be
+    applied to ensure mass conservation during evolution.
+
+    Parameters
+    ----------
+    df: pd.DataFrame
+        Dataframe containing stellar models.
+
+    Returns
+    -------
+    pd.DataFrame
+        The input dataframe, with rescale chemical composition of there were small errors.
+
+    Raises
+    ------
+    ValueError: If cc models are not present, or unknown models types are present.
+    """
     elements = el2z.keys()
     elements = list(set(df.columns).intersection(set(elements)))
 
     check = df[elements].sum(axis=1)
     diff = abs(check - 1.0)
 
-    if diff.max() >= 1.0e-3:
+    if diff.max() <= 1.0e-3:
+        # Allow scaling of mass fractions. Sometimes there are rounding errors in data tables, etc.
+        scale = 1 / check
+
+        df.loc[:, elements] = df[elements].mul(scale, axis="rows")
+    elif diff.max() <= 1.0e-2:
         warnings.warn(
             f"Some stellar models have significant errors ({np.round(diff.max(), 3)}) "
-            f"in sum(mass fractions)."
+            f"in sum(mass fractions). Evolution will likely fail to conserve mass."
         )
-
-    # Allow scaling of mass fractions. Sometimes there are rounding errors in data tables, etc.
-    scale = 1 / check
-
-    df.loc[:, elements] = df[elements].mul(scale, axis="rows")
-
-    check2 = abs(df[elements].sum(axis=1) - 1.0)
-    if check2.max() >= 1.0e-12:
-        raise error_handling.ProgramError("Unable to scale mass fractions.")
+    else:
+        raise ValueError(
+            f"Large errors ({np.round(diff.max(), 3)}) exist in "
+            f"sum(mass fractions) for input data. Should sum to one."
+        )
 
     return df
 
 
 def check_mass_metallicity_consistent(df: pd.DataFrame) -> None:
+    """
+    Each model of mass `m` must have the same set of metallicities `z`.
+
+    Parameters
+    ----------
+    df: pd.DataFrame
+        Dataframe containing stellar models.
+
+    Raises
+    ------
+    ValueError: If metallicities are not consistent between models.
+    """
     z_list = df.Z.unique()
     m_list = df.mass.unique()
 
     for m in m_list:
         z_ = df[df.mass == m].Z.unique()
         if sorted(z_) != sorted(z_list):
-            raise error_handling.NotImplementedError()
+            raise ValueError(
+                f"Error in models with mass {m}. Each model of mass `m` must have the same "
+                f"set of metallicities (Z)."
+            )
 
 
 def check_wind_component(df: pd.DataFrame) -> pd.DataFrame:
     """
-    Ensure that CC/HN models where mass != mass_final, that a row is added
-    to store wind composition
+    Ensure that each CC/HN model has a corresponding row for the wind composition.
+    If any are missing, add a row, where the composition will be filled during the evolution
+    calculation with the ISM composition at the time of birth.
+
+    Parameters
+    ----------
+    df: pd.DataFrame
+        Dataframe containing stellar models.
+
+    Returns
+    -------
+    pd.DataFrame
+        The input dataframe, with wind rows added if they were missing.
     """
     elements = list(set(df.columns).intersection(set(el2z.keys())))
 
-    for i, model in df[(df.type == "cc")].iterrows():  # & (df.mass != df.mass_final)].iterrows():
+    for i, model in df[(df.type == "cc")].iterrows():
         if "wind" not in df[(df.mass == model.mass) & (df.Z == model.Z)].type:
             new_model = model.copy()
             new_model["type"] = "wind"
@@ -197,9 +286,7 @@ def check_wind_component(df: pd.DataFrame) -> pd.DataFrame:
             df = df.append(new_model, ignore_index=True)
 
     if config.STELLAR_MODELS["include_hn"] is True:
-        for i, model in df[
-            (df.type == "hn")
-        ].iterrows():  # & (df.mass != df.mass_final)].iterrows():
+        for i, model in df[(df.type == "hn")].iterrows():
             if "hn_wind" not in df[(df.mass == model.mass) & (df.Z == model.Z)].type:
                 new_model = model.copy()
                 new_model["type"] = "hn_wind"
@@ -211,15 +298,33 @@ def check_wind_component(df: pd.DataFrame) -> pd.DataFrame:
 
 def check_hn_models(df: pd.DataFrame) -> pd.DataFrame:
     """
-    If HNe models are included, there masses must be either; (i) a subset
-    of the SNe models, or, (ii) intersect with the upper end of SNe masses.
-    The chemical contribution from each mass above the minimum HN mass
-    is a weighted average of the HN and SN models. If (ii), then then SNe
-    masses will be extended to match the upper mass of HNe models, but added
-    sn models added will be assumed to collapse directly to BH.
-    E.g., (i) would be masses sn: [10, 15, 20, 30] and hn: [20, 30].
-    (ii) would be masses sn: [10, 15, 20, 30] and hn: [20, 30, 40, 60],
-    and the sn masses would be extended to [10, 15, 20, 30, 40, 60].
+    If HNe models are included, there masses must be either;
+    (i) a subset of the SNe models, or,
+    (ii) intersect with the upper end of SNe masses.
+
+    The chemical contribution from each mass above the minimum HN mass is calculated as a
+    weighted average of the HN and SN models. If the case is (ii), then then SNe masses
+    will be extended to match the upper mass of HNe models, but the additional sn models
+    will be assumed to collapse to BH without ejecta.
+
+    Example; case (i) might be masses sn: [10, 15, 20, 30] and hn: [20, 30].
+    Case (ii) might be masses sn: [10, 15, 20, 30] and hn: [20, 30, 40, 60], and the sn
+    masses would be extended with two additional models, [10, 15, 20, 30, 40, 60], where
+    the 40 and 60 solar mass sn models would had remnant mass == ZAMS mass.
+
+    Parameters
+    ----------
+    df: pd.DataFrame
+        Dataframe containing stellar models.
+
+    Returns
+    -------
+    pd.DataFrame
+        The input dataframe, with high mass sn models added if necessary to match hn masses.
+
+    Raises
+    ------
+    ValueError: If cc models masses are incompatible with the hn model masses.
     """
 
     elements = el2z.keys()
@@ -239,9 +344,7 @@ def check_hn_models(df: pd.DataFrame) -> pd.DataFrame:
         )
 
     # Extend SN models to match upper mass of HN models.
-    # New SN models have same properties as corresponding HN model, but
-    # ejecta mass will be zero. Setting ejecta composition should not be
-    # necessary, but do so anyway to be safe.
+    # New SN models have same properties as corresponding HN model, but ejecta mass will be zero.
     if mass_max_sn < np.max(mass_hn):
         extend_sn = pd.DataFrame(df[(df.type == "hn") & (df.mass > mass_max_sn)])
         extend_sn["remnant_mass"] = list(extend_sn.mass)
